526b8b50eeb628124721bde3c144d8b57cfa2a4a
Noah Tatpati <01fe25bca089@kletech.ac.in>
2026-09-07 14:18:49 +0530

    Updated commit

--- Greeting.py
+++ Greeting.py
@@ -1,6 +1,2 @@
 name = input("Enter your name: ")
-<<<<<<< HEAD
-print("Hello, ",name,"Welcome")
-=======
-print("Hello, ",name,"Welcome!")
->>>>>>> 6d4fe7192bd1f42c26116595ea985c946107b8d7
+print("Hello, ",name," Welcome")
